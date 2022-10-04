6ac185edf75946205b9957c9b73d1af4c3ebf92b
Aamir <aamir.bashir@codebrewinnovations.com>
2022-10-04 12:00:39 +0530

    separation of sdk

--- android/app/src/main/java/com/app/unl_project/MainActivity.kt
+++ android/app/src/main/java/com/app/unl_project/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.app.unl_project
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.app.unl_map_sdk.UnlMap
+import com.app.unl_map_sdk.UnlMapView
+import com.mapbox.mapboxsdk.annotations.PolygonOptions
+import com.mapbox.mapboxsdk.geometry.LatLng
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         UnlMap(applicationContext,getString(R.string.api_key),getString(R.string.vpm_id))
         setContentView(R.layout.activity_main)
-
+        var unlMap=findViewById<UnlMapView>(R.id.mapView)
+        unlMap.getMapAsync {
+            var poly = ArrayList<LatLng>()
+            poly.add(LatLng(34.126256, 74.832149))
+            poly.add(LatLng(34.126505, 74.835068))
+            poly.add(LatLng(34.122597, 74.836956))
+            poly.add(LatLng(34.123307, 74.832536))
+            it.addPolygon(PolygonOptions()
+                .addAll(poly)
+                .fillColor(Color.parseColor("#202020")))
+        }
     }
 }
