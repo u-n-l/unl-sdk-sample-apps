abcf58c282036485dd42bb34c504b7f5144a8a14
Aamir <aamir.bashir@codebrewinnovations.com>
2022-12-09 14:35:33 +0530

    update package name

--- android/app/src/main/java/com/app/unl_project/MainActivity.kt
+++ android/app/src/main/java/com/app/unl_project/MainActivity.kt
@@ -3,11 +3,11 @@ package com.app.unl_project
 import android.os.Bundle
 import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
-import com.app.unl_map_sdk.UnlMap
-import com.app.unl_map_sdk.helpers.grid_controls.setGridControls
-import com.app.unl_map_sdk.views.UnlMapView
-import com.app.unl_map_sdk.helpers.tile_controls.enableTileSelector
-import com.app.unl_map_sdk.helpers.tile_controls.setTileSelectorGravity
+import com.unl.map.sdk.UnlMap
+import com.unl.map.sdk.helpers.grid_controls.setGridControls
+import com.unl.map.sdk.helpers.tile_controls.enableTileSelector
+import com.unl.map.sdk.helpers.tile_controls.setTileSelectorGravity
+import com.unl.map.sdk.views.UnlMapView
 
 class MainActivity : AppCompatActivity() {
 
