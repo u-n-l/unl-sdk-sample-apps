ae1455eb5b679a6fbff95d0c812c19c29c863f70
Gurwinder Singh <gurwinder@unl.global>
2023-04-06 18:56:55 +0530

    Added indoor

--- ios/SampleApp/Constants.swift
+++ ios/SampleApp/Constants.swift
@@ -8,9 +8,5 @@
 import Foundation
 import UIKit
 
-let BASE_URL = "https://alpha.platform.unl.global/"
-//let KEY_API = "TyWCPuvtqc4xAE8jGZX6IWKnPRpWKQWf"
-//let ID_VPM = "b96e28f6-a262-42e9-a337-a54ea90090f8"
-
 let KEY_API = "rxiVTQsv6ByJixVgU1NLbMitqXcvjYSQ"
 let ID_VPM = "5a38e77b-dd5e-46f7-ae9d-5f3aee3c4a94"
