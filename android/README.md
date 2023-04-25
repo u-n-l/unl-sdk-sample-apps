3ab14df95aba283913e419e8319f4daf82656e47
Lokeshpunl <lokesh@unl.global>
2023-04-25 14:56:54 +0530

    updated android app

--- android/README.md
+++ android/README.md
@@ -1,1 +1,9 @@
 # unl-sdk-sample-apps
+
+-You can pass vpm_id and api_key with Environment parameter and initiate the UNL MAP SDK
+UnlMap(applicationContext,getString(R.string.api_key),getString(R.string.vpm_id), EnvironmentType.SANDBOX)
+
+-Set API Key and VPM ID in String File in values folder 
+
+ <string name="api_key">UNL_API_KEY</string>
+ <string name="vpm_id">UNL_VPM_ID</string>
--- android/app/src/main/java/com/app/unl_project/MainActivity.kt
+++ android/app/src/main/java/com/app/unl_project/MainActivity.kt
@@ -3,24 +3,31 @@ package com.app.unl_project
 import android.os.Bundle
 import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
-import com.app.unl_map_sdk.UnlMap
-import com.app.unl_map_sdk.helpers.grid_controls.setGridControls
-import com.app.unl_map_sdk.views.UnlMapView
-import com.app.unl_map_sdk.helpers.tile_controls.enableTileSelector
-import com.app.unl_map_sdk.helpers.tile_controls.setTileSelectorGravity
+import com.unl.map.sdk.UnlMap
+import com.unl.map.sdk.data.EnvironmentType
+import com.unl.map.sdk.helpers.grid_controls.setGridControls
+import com.unl.map.sdk.helpers.tile_controls.enableTileSelector
+import com.unl.map.sdk.helpers.tile_controls.setTileSelectorGravity
+import com.unl.map.sdk.views.UnlMapView
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        UnlMap(applicationContext,getString(R.string.api_key),getString(R.string.vpm_id))
+        UnlMap(applicationContext,getString(R.string.api_key),getString(R.string.vpm_id),
+            EnvironmentType.SANDBOX)
         setContentView(R.layout.activity_main)
         var unlMap=findViewById<UnlMapView>(R.id.mapView)
         unlMap.getMapAsync {
             unlMap.fm=supportFragmentManager
+            unlMap.activity=this
+            unlMap.lifeCycleOwner=this
+            unlMap.enableIndoorMap=true
+            unlMap.viewLifecycle=this
             unlMap.enableTileSelector(true)
             unlMap.setGridControls(this,true)
-            unlMap.setTileSelectorGravity(Gravity.RIGHT)
+            unlMap.setTileSelectorGravity(Gravity.END)
         }
     }
 }
